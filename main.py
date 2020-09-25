bf8f8a09e551726b921dadae8f61b2ed48772aed
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-09-24 20:13:25 -0500

    code clean-up

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ def main():
 
 def inputs():
     new_name = input('Artist name: ')
-    name_query =  Artists.select().where(Artists.artist == new_name)
+    name_query = Artists.select().where(Artists.artist == new_name)
     if name_query.count() == 0:
-        email = input('new_email:' )
-        noa = Artists(artist = new_name, email = email)
+        email = input('new_email:')
+        noa = Artists(artist=new_name, email=email)
         noa.save()
     artwork = input("Enter the name of artwork: ")
     new_price = float(input('Enter price: '))
