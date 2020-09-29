1f1d913b89c54c6fb6bb6220fc00a35b033fa48d
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-09-29 10:56:29 -0500

    moved database set up to a seperate file and connected database

--- artist_db.py
+++ artist_db.py
@@ -1,4 +1,30 @@
 from peewee import *
 from config import db_path
 
-db = db_path
+db = SqliteDatabase(db_path)
+
+#TODO split tables and classes into seperate file
+class Artists(Model):
+    artist = CharField()
+    email = CharField()
+
+    class Meta:
+        database = db
+
+    def __str__(self):
+        return f'Name: {self.artist} | email: {self.email}'
+
+class Artworks(Model):
+    artist = ForeignKeyField(Artists, backref= 'works')
+    artwork_name = CharField()
+    price = DecimalField()
+    available = CharField(default='Available')
+
+    class Meta:
+        database = db
+
+    def __str__(self):
+        return f'Artist: {self.artist} | Name: {self.artwork_name} | Price: {self.price} | Available: {self.available}'
+
+db.connect()
+db.create_tables([Artists, Artworks])
--- main.py
+++ main.py
@@ -1,35 +1,10 @@
 from peewee import *
+from artist_db import Artists, Artworks
 db = SqliteDatabase('gallery.sqlite')
-
-#TODO split tables and classes into seperate file
-class Artists(Model):
-    artist = CharField()
-    email = CharField()
-
-    class Meta:
-        database = db
-
-    def __str__(self):
-        return f'Name: {self.artist} | email: {self.email}'
-
-class Artworks(Model):
-    artist = ForeignKeyField(Artists, backref= 'works')
-    artwork_name = CharField()
-    price = DecimalField()
-    available = CharField(default='Available')
-
-    class Meta:
-        database = db
-
-    def __str__(self):
-        return f'Artist: {self.artist} | Name: {self.artwork_name} | Price: {self.price} | Available: {self.available}'
-
-db.connect()
-db.create_tables([Artists, Artworks])
-
 
 def main():
     print('Gallery database')
+
     while True:
         print_menu()
         choice = get_menu_choice()
