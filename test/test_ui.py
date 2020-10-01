00c5a57ed7a07a7ad5dc35b3e86a8f4c6d7a1f27
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-10-01 14:04:39 -0500

    adding functionality to test_ui

--- test/test_ui.py
+++ test/test_ui.py
@@ -1,10 +1,16 @@
-from unittest import TestCase
+from unittest import TestCase, mock
+from unittest.mock import patch
 import ui
 
 
 class TestUI(TestCase):
 
-    def test_get_name(self):
+    #mock is used to provide mock user inputs to test values
+    @mock.patch('ui.get_name', return_value='bbb')
+    def test_get_name(self, mock_input):
+        # mocked_input.side_effect = ['bbb']
+        result = ui.get_name()
+        self.assertEqual(result, 'bbb')
         pass
 
     def test_get_name_no_entry(self):
