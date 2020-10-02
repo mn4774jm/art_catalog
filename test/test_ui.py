b87f7da7ced278663ca15a7812c6813d0d2c097e
Thomas Mullins <mn4774jm@go.minneapolis.edu>
2020-10-02 14:16:47 -0500

    initial unit test built and passing in test_ui

--- test/test_ui.py
+++ test/test_ui.py
@@ -36,26 +36,27 @@ class TestUI(TestCase):
         self.assertEqual(result, 'bbb@gmail.com')
         pass
 
-    @mock.patch('builtins.input', side_effect=['////', 'bbb'])
+    @mock.patch('builtins.input', side_effect=['qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm@gmail.com', 'bbb@gmail.com'])
     def test_get_email_too_many_characters(self, mock_input):
-        pass
+        result = ui.get_email()
+        self.assertEqual(result, 'bbb@gmail.com')
 
-    @mock.patch('builtins.input', side_effect=['////', 'bbb'])
+    @mock.patch('builtins.input', side_effect=['@gmail.bbb', 'bbb@gmail.com'])
     def test_get_email_not_correct_format(self, mock_input):
-        pass
+        result = ui.get_email()
+        self.assertEqual(result, 'bbb@gmail.com')
 
-    @mock.patch('builtins.input', side_effect=['////', 'bbb'])
+    @mock.patch('builtins.input', side_effect=['400'])
     def test_get_value(self, mock_input):
-        pass
+        result = ui.get_value()
+        self.assertEqual(result, 400)
 
-    @mock.patch('builtins.input', side_effect=['////', 'bbb'])
+    @mock.patch('builtins.input', side_effect=['sdfg', '400'])
     def test_get_value_not_numeric(self, mock_input):
-        pass
+        result = ui.get_value()
+        self.assertEqual(result, 400)
 
-    @mock.patch('builtins.input', side_effect=['////', 'bbb'])
-    def test_remove_art_check_in_database(self, mock_input):
-        pass
-
-    @mock.patch('builtins.input', side_effect=['////', 'bbb'])
-    def test_remove_art_not_in_database(self, mock_input):
-        pass
+    @mock.patch('builtins.input', side_effect=['Y'])
+    def test_remove_art_option(self, mock_input):
+        result = ui.remove_art_check('art_name')
+        self.assertEqual(result, 'y')
